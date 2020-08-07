2a5396eef072000e2609003f452f82cca90cf120
小乌嘎 <xiaowuga@MacBook-Pro.local>
2020-08-07 14:05:15 +0800

    change listnode_insertaspred.h

---
+++ list/listnode_insertaspred.h
@@ -0,0 +1,10 @@
+#include "listnode.h"
+
+template <typename T>
+ListNodePosi(T) ListNode<T>::insertAsPred(T const& e) 
+{
+    ListNodePosi(T)x = new ListNode(e, pred, this);
+    pred->secc = x;
+    pred = x;
+    return x;
+}
