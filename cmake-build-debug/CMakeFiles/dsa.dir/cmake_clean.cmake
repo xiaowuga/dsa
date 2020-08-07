52d5ff0363b68733c0c0b73ca7bc419ce2236b29
xiaowuga <729918169@qq.com>
2020-08-07 17:14:05 +0800

    add something

---
+++ cmake-build-debug/CMakeFiles/dsa.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dsa.dir/main.cpp.o"
+  "dsa"
+  "dsa.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dsa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
